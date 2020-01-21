92710da525b26b550f58cad12b4b3f092061c59b
Alexsandro Deitos <alexdeitos01@gmail.com>
2020-01-21 15:50:45 -0300

    ajuste exe05 udemy

--- Udemy/Exe05PythonBasico.py
+++ Udemy/Exe05PythonBasico.py
@@ -8,12 +8,12 @@ valor2 = int(input("Digite o valor 2: "))
 if sinal == '+':
     resultado = valor1 + valor2
     print("soma: ", resultado)
-if sinal == '-':
+elif sinal == '-':
     resultado = valor1 - valor2
     print("subtração: ", resultado)
-if sinal == '/':
+elif sinal == '/':
     resultado = valor1 / valor2
     print("divisao: ", resultado)
-if sinal == '*':
+elif sinal == '*':
     resultado = valor1 * valor2
     print("multiplicação: ", resultado)
